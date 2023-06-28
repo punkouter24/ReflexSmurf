5c273b7d3e5a12bd6896f02e8f4b1eec4c6a32ca
Matthew Herb <punkouter23@outlook.com>
2023-06-28 11:27:58 -0400

    ...

--- ScoreBoardPage.xaml.cs
+++ ScoreBoardPage.xaml.cs
@@ -2,7 +2,7 @@ namespace ReflexSmurf;
 
 public partial class ScoreBoardPage : ContentPage
 {
-    private readonly ScoreService scoreService = new();
+   // private readonly ScoreService scoreService = new();
 
     private List<Score> highScores;
 
@@ -14,7 +14,7 @@ public partial class ScoreBoardPage : ContentPage
 
     private void LoadScores()
     {
-        highScores = scoreService.LoadScores();
-        ScoreListView.ItemsSource = highScores.OrderBy(s => s.Value).Take(10);
+       // highScores = scoreService.LoadScores();
+        //ScoreListView.ItemsSource = highScores.OrderBy(s => s.Value).Take(10);
     }
 }
